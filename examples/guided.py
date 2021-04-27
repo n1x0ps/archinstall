9abb241c9b8d93e5f432163e906c6c6ecf50d362
Yash Tripathi <tripathiyash97@gmail.com>
2021-04-27 20:20:41 +0530

    creating profile object from path

--- examples/guided.py
+++ examples/guided.py
@@ -401,6 +401,6 @@ else:
 	# Temporarily disabling keep_partitions if config file is loaded
 	archinstall.arguments['harddrive'].keep_partitions = False
 	# Profile object should be created in perform_installation_steps
-	archinstall.arguments['profile'] = Profile(archinstall.arguments['profile']['path'].split('/')[-1].split('.')[0])
+	archinstall.arguments['profile'] = Profile(path=archinstall.arguments['profile']['path'])
 
 perform_installation_steps()
